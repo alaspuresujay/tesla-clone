ae1ab51e2a3f27336de67ee47247f4fd85251494
alaspuresujay <alaspuresujay@gmail.com>
2021-04-12 23:07:06 +0530

    Added Framer Motion

---
+++ src/data.js
@@ -0,0 +1,95 @@
+// @ts-nocheck
+import Accessories from './assets/Desktop-Accessories.jpg';
+import model3 from './assets/Desktop-Model3.jpeg';
+import Models from './assets/Desktop-ModelS.jpeg';
+import ModelX from './assets/Desktop-ModelX.jpeg';
+import ModelY from './assets/Desktop-ModelY.jpeg';
+import SolarPanel from './assets/Desktop-SolarPanels.jpeg';
+import SolarRoof from './assets/Desktop-SolarRoof.jpeg';
+
+export default [
+	{
+		title: 'Model S',
+		desc: 'Super Car',
+		descLink: '',
+		backgroundImg: Models,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: true,
+		first: true,
+	},
+	{
+		title: 'Model 3',
+		desc: 'Best Car till Date',
+		descLink: '',
+		backgroundImg: model3,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: true,
+		first: false,
+	},
+	{
+		title: 'Mdoel X',
+		desc: 'Best Car till Date',
+		descLink: '',
+		backgroundImg: ModelX,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: true,
+		first: false,
+	},
+	{
+		title: 'Model Y',
+		desc: 'Best Car till Date',
+		descLink: '',
+		backgroundImg: ModelY,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: true,
+		first: false,
+	},
+	{
+		title: 'Lowest Cost Solar Panel in America',
+		desc: 'Best Car till Date',
+		descLink: '',
+		backgroundImg: SolarPanel,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: true,
+		first: false,
+	},
+	{
+		title: 'Lowest Cost Solar Roof in America',
+		desc: 'Solar Roof Costs Less Than a New Roof Plus Solar Panels',
+		descLink: '',
+		backgroundImg: SolarRoof,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: true,
+		first: false,
+	},
+	{
+		title: 'Lowest cost Accessories in America',
+		desc: 'Best Car Accessories in the World',
+		descLink: '',
+		backgroundImg: Accessories,
+		leftBtnTxt: 'ORDER NOW',
+		leftBtnLink: '',
+		rightBtnTxt: 'LEARN MORE',
+		rightBtnLink: '',
+		twoButton: false,
+		first: false,
+	},
+];
